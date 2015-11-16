f572b1858ace44f73edf0acac3b987b4bd6f76ed
Wouter Bolhuis <wouter.bolhuis@gmail.com>
2015-11-16 15:10:50 +0100

    Revert "Change all the damn things"
    This reverts commit 0d39122481c8fc1d4dbab5756812243db15b53e1.

--- GIT SS/src/Hello.java
+++ GIT SS/src/Hello.java
@@ -1,14 +1,8 @@
 
 public class Hello {
 	
-	private String message;
-	
-	public void print(){
-		System.out.println(message);
-	}
-	
-	public void change(String text){
-		message = text;
+	public static void main (String[] args){
+		System.out.println("Haai");
 	}
 	
 }
