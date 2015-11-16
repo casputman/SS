c07bb14fbfe15c6824d164bb749383dab6867837
cas-p <cas-p@192.168.178.15>
2015-11-16 18:18:31 +0100

    Meer woorden!

--- GIT SS/src/Hello3.java
+++ GIT SS/src/Hello3.java
@@ -2,6 +2,6 @@
 public class Hello3 {
 
 		public static void main(String[] args) {
-			System.out.println("Het beginnen er wel veel te worden");
+			System.out.println("Het beginnen er wel veel te worden, helemaal nu ze zijn aangepast.");
 		}
 }
